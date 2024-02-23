a6d4ba484c5e9e90cc43f437415464ea40fd5ffe
Iamam786 <abdulrahimali1998@gmail.com>
2024-02-23 20:35:43 +0530

    Mobile responsive

--- script.js
+++ script.js
@@ -24,15 +24,37 @@ carousel();
 
 let dropdown = document.getElementsByClassName('dropdown');
 
-document.querySelector('ul li.eventlistner').addEventListener('mouseover', function() {
-   for (let i = 0; i < dropdown.length; i++) {
-    dropdown[i].style.display = 'block';
-   }
+document.querySelector('ul li.eventlistner').addEventListener('mouseover', function () {
+    for (let i = 0; i < dropdown.length; i++) {
+        dropdown[i].style.display = 'block';
+    }
 })
 
-document.querySelector('ul li.eventlistner').addEventListener('mouseout', function() {
-   for (let i = 0; i < dropdown.length; i++) {
-    dropdown[i].style.display = 'none';
-   }
+document.querySelector('ul li.eventlistner').addEventListener('mouseout', function () {
+    for (let i = 0; i < dropdown.length; i++) {
+        dropdown[i].style.display = 'none';
+    }
 })
 
+
+// menubar
+
+function menubar() {
+    document.querySelector(' li:last-child .show').style.display = "none";
+    document.querySelector(' li:last-child .hide').style.display = "block";
+    document.querySelector('nav ul').style.flexDirection = "column";
+    document.querySelector('nav ul li').style.display = "block";
+}
+function menubarHide() {
+    document.querySelector(' li:last-child .hide').style.display = "node";
+    document.querySelector(' li:last-child .show').style.display = "block";
+}
+
+// function menuToggle() {
+//     let toggle = document.getElementById('menuToggle');
+//     let navigation = document.querySelector('.navigation')
+//     let li = document.querySelectorAll('li');
+//     toggle.classList.toggle('active');
+//     navigation.classList.toggle('active');
+//     li.style.display = "block"
+// }
